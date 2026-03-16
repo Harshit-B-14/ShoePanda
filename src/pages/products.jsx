c95822e7cca4d756e0018bc3a2ee5f1a7ab0f78c
Harshit_2 <bhandariharshit14@gmail.com>
2026-03-16 17:50:04 +0530

    added search products feature in products page

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import ProductCard from "../components/productCard";
 import ProductList from "../assets/productList";
 
 function Products(){
+
+    const [search, setSearch] = useState("");
+
     return <>
-        {/* <Header></Header> */}
+        <input className="searchBox" placeholder="Search for products" onChange={(query) => setSearch(query.target.value)}></input>
         <div className="Products">
-            {ProductList.map((product) => {
-                return <Link to={`/productDetails/${product.id}`} key = {product.id}>
-                    <ProductCard 
-                        name = {product.name} 
-                        image = {product.image} 
-                        price = {product.price}>
-                    </ProductCard>
-                </Link>
-            })}
+            {ProductList.filter((product) => 
+                product.name.toLowerCase().includes(search.toLowerCase())).map((product) => 
+                {
+                    return <Link to={`/productDetails/${product.id}`} key = {product.id}>
+                        <ProductCard 
+                            name = {product.name} 
+                            image = {product.image} 
+                            price = {product.price}>
+                        </ProductCard>
+                    </Link>
+                })}
         </div>
         {/* <Footer></Footer> */}
     </>
