ef47bd1bc958a42d58554a732b36bd043ceab25e
Harshit_2 <bhandariharshit14@gmail.com>
2026-03-30 23:17:55 +0530

    made contact page mobile responsive by adding styling

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,14 @@
 function Contact(){
     return <div className="contact">
-        <h1 className="contact-heading">Contact Us</h1>
-        <p>Have questions about your order or products? We're here to help.</p>
+        <h2 className="contact-heading">Contact Us</h2>
+        <p className="non-important-p">Have questions about your order or products? We're here to help.</p>
         <div className="contact-details">
-        <p className="important-p"><strong>Email : </strong>shoepanda.shop@gmail.com</p>
-        <p className="important-p"><strong>Phone : </strong>+91 9847382955</p>
-        <p className="important-p"><strong>Address : </strong>Delhi, India</p>
+        <p className="important-p">Email : <strong>shoepanda.shop@gmail.com</strong></p>
+        <p className="important-p">Phone : <strong>+91 9847382955</strong></p>
+        <p className="important-p">Address : <strong>Delhi, India</strong></p>
         </div>
 
-            <p>We usually reply within 24 hours</p>
+            <p className="non-important-p">We usually reply within 24 hours</p>
         <div className="contact-follow">
             <p className="important-p"><strong>Follow us : </strong></p>
             <p className="important-p" style={{textAlign: "center" ,whiteSpace: "pre"}}>Instagram  |  LinkedIn  |  Twitter</p>
