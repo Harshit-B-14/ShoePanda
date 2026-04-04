834a3cca301ed21314b7a0e51e18a17b1c1b9396
Harshit <bhandariharshit14@gmail.com>
2026-04-04 13:39:46 +0530

    added hero section made by me

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import ProductCard from "../components/productCard";
 import ProductList from "../assets/productList";
-import hero from "../assets/Hero.avif"
+import hero from "../assets/Hero.png"
 import {Link} from "react-router-dom"
 
 function Home(){
